9e5273933215ba370745207af4a2fc35be0710d3
Joyal James <75773527+joyaljms98@users.noreply.github.com>
2025-02-14 06:59:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function updateTime() {
     document.getElementById("date").innerText = now.toLocaleDateString("en-US", options);
 
     // Format Time
-    let time = now.toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    let time = now.toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit'});
     document.getElementById("time").innerText = time;
           // to also get the seconds :- let time = now.toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 }
